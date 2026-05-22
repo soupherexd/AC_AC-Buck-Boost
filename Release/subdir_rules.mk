840cab06fa231272552d8c4d6ea4d6a9dd4c9b24
souphere <souphere@qq.com>
2026-05-22 10:30:14 +0800

    GB2312 to UTF-8

--- Release/subdir_rules.mk
+++ Release/subdir_rules.mk
@@ -8,7 +8,7 @@ SHELL = cmd.exe
 %.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: C2000 Compiler'
-	"C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/bin/cl2000" -v28 -ml -mt --cla_support=cla0 --float_support=fpu32 --vcu_support=vcu0 -O2 --include_path="C:/Users/souph/Documents/CCSProjects/BuckPara/EXAMPLE_SPWM" --include_path="D:/work/CCS/TMS320F28069/F2806x_common/include" --include_path="D:/work/CCS/TMS320F28069/F2806x_headers/include" --include_path="C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include" --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/bin/cl2000" -v28 -ml -mt --cla_support=cla0 --float_support=fpu32 --vcu_support=vcu0 -O2 --include_path="C:/Users/souph/Documents/CCSProjects/ACAC/AC_AC Buck-Boost" --include_path="D:/work/CCS/TMS320F28069/F2806x_common/include" --include_path="D:/work/CCS/TMS320F28069/F2806x_headers/include" --include_path="C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include" --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
