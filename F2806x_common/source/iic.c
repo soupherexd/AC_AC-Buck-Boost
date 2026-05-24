bbb3e9cd8ce4d5e9abb94f63cbca69ea1f0fb282
souphere <souphere@qq.com>
2026-05-24 13:42:29 +0800

    SSD1315适配（已通过）

--- F2806x_common/source/iic.c
+++ F2806x_common/source/iic.c
@@ -1,6 +1,5 @@
 #include "iic.h"
-void I2C_Ack3(void)
-{
+void I2C_Ack3(void) {
     resetSDA;
     DELAY_US(1);
     setSCL;
@@ -9,9 +8,7 @@ void I2C_Ack3(void)
     DELAY_US(1);
 }
 
-
-void I2C_NAck3(void)
-{
+void I2C_NAck3(void) {
     setSDA;
     DELAY_US(1);
     setSCL;
@@ -20,9 +17,7 @@ void I2C_NAck3(void)
     DELAY_US(1);
 }
 
-
-void I2C_Start3(void)
-{
+void I2C_Start3(void) {
     setSDA;
     DELAY_US(1);
     setSCL;
@@ -32,11 +27,10 @@ void I2C_Start3(void)
     resetSCL;
     DELAY_US(1);
     OLED_WriteByte(0x78);
-    I2C_Ack3();//*这句很重要
+    I2C_Ack3();  //*这句很重要
 }
 
-void I2C_Stop3(void)
-{
+void I2C_Stop3(void) {
     setSCL;
     DELAY_US(1);
     resetSDA;
@@ -45,19 +39,13 @@ void I2C_Stop3(void)
     DELAY_US(1);
 }
 
+void OLED_WriteByte(Uint8 mcmd) {
+    Uint8 length = 8;  // Send Command
 
-void OLED_WriteByte(Uint8 mcmd)
-{
-    Uint8 length = 8;           // Send Command
-
-    while(length--)
-    {
-        if(mcmd & 0x80)
-        {
+    while (length--) {
+        if (mcmd & 0x80) {
             setSDA;
-        }
-        else
-        {
+        } else {
             resetSDA;
         }
         DELAY_US(1);
@@ -69,4 +57,36 @@ void OLED_WriteByte(Uint8 mcmd)
     }
 }
 
-//2024.3.15 由搁浅飞云改进
+/*
+ * I2C总线释放函数：发送9个时钟脉冲
+ * 用于释放卡在低电平的SDA线（从机可能处于错误状态）
+ */
+void I2C_ReleaseBus(void) {
+    Uint8 i;
+
+    /* 先确保SCL和SDA均为高 */
+    setSDA;
+    DELAY_US(5);
+    setSCL;
+    DELAY_US(5);
+
+    /* 发送最多9个时钟脉冲，尝试释放总线 */
+    for (i = 0; i < 9; i++) {
+        resetSCL;
+        DELAY_US(5);
+        setSCL;
+        DELAY_US(5);
+    }
+
+    /* 产生一个STOP条件使总线回到空闲状态 */
+    resetSCL;
+    DELAY_US(5);
+    resetSDA;
+    DELAY_US(5);
+    setSCL;
+    DELAY_US(5);
+    setSDA;
+    DELAY_US(5);
+}
+
+// 2024.3.15 由搁浅飞云改进
